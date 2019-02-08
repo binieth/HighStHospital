9f4170ea7d67b57835b8a8d944541a8a971ff52b
Bini <abigiyabiniyam@gmail.com>
2019-02-08 14:13:28 -0500

    first message

---
+++ src/main/java/highsthospital/Employee.java
@@ -0,0 +1,19 @@
+package highsthospital;
+
+public class Employee {
+	String doctor;
+	String nurse;
+	String surgeon;
+	String receptionist;
+	String janitor;
+	public Employee(String doctor, String nurse, String surgeon, String receptionist, String janitor) {
+		
+		this.doctor = doctor;
+		this.nurse = nurse;
+		this.surgeon = surgeon;
+		this.receptionist = receptionist;
+		this.janitor = janitor;
+	}
+	
+
+}
