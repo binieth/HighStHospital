d9436d07191ddedec9cf2880b8228f39cf443082
Bini <abigiyabiniyam@gmail.com>
2019-02-08 15:02:58 -0500

    nothing

--- src/main/java/highsthospital/Patient.java
+++ src/main/java/highsthospital/Patient.java
@@ -1,5 +1,15 @@
 package highsthospital;
 
-public class Patient {
+public class Patient extends Hospital {
+
+	public Patient(String employees, String patients) {
+		super(employees, patients);
+		 
+			
+	}
+	
+	private final int BLOOD_LEVEL = 20;
+	private final int HEALTH_LEVEL = 10;
+String name;
 
 }
