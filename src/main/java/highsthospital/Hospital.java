4ea60b3af879a842187af8e5c6dd8dedd9696ab4
Bini <abigiyabiniyam@gmail.com>
2019-02-08 14:47:14 -0500

    commit message 1

---
+++ src/main/java/highsthospital/Hospital.java
@@ -0,0 +1,17 @@
+package highsthospital;
+
+public class Hospital {
+	
+	private String employees;
+	private String patients;
+	
+	public Hospital(String employees, String patients) {
+	
+		this.employees = employees;
+		this.patients = patients;
+	}
+	
+	public void 
+	
+
+}
